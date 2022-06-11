918d3299d63432d55d236aa3938b92813050725f
jang1er <jokruger03@gmail.com>
2022-06-11 18:49:59 +0200

    New comment

--- platformer_2d/src/Main/Game.java
+++ platformer_2d/src/Main/Game.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class Game {
 
 	public static void main(String[] args) {
-		// generate new game window
+		// generate new game window (320 x 240)
 	    JFrame window = new JFrame("Platformer Test");
 	    window.setContentPane(new GamePanel());
 	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
