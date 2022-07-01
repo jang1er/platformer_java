5b7d9b2e658475d1d55121bdb188402342ebde32
TheJoooo <107306494+TheJoooo@users.noreply.github.com>
2022-07-01 18:10:24 +0200

    Part 2 Finished

--- platformer_2d/src/GameState/Level1State.java
+++ platformer_2d/src/GameState/Level1State.java
@@ -1,8 +1,8 @@
 package GameState;
 
 import java.awt.*;
-
-import Tilemap.TileMap;
+import Main.GamePanel;
+import Tilemap.*;
 
 public class Level1State extends GameState {
 
@@ -14,12 +14,24 @@ public class Level1State extends GameState {
 	
 	public void init() {
 		tileMap = new TileMap(30);
+		tileMap.loadTiles("/Tilesets/grasstileset.gif");
+		tileMap.loadMap("/Mpas/level1-1.map");
+		tileMap.setPosition(0, 0);
 		
 	}
 	
 	
 	public void update() {}
-	public void draw(Graphics2D g) {}
+	public void draw(Graphics2D g) {
+	
+		//clear screen
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
+		
+		//draw tilemap
+		tileMap.draw(g);
+	}	
+	
 	public void keyPressed(int k ) {}
 	public void keyReleased(int k) {}
 
