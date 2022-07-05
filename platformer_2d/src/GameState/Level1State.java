dc0baf31ac697b78679be8694fcae619997d7896
TheJoooo <107306494+TheJoooo@users.noreply.github.com>
2022-07-06 00:06:40 +0200

    Level 1 Hintergrund hinzugefügt

--- platformer_2d/src/GameState/Level1State.java
+++ platformer_2d/src/GameState/Level1State.java
@@ -7,6 +7,7 @@ import Tilemap.*;
 public class Level1State extends GameState {
 
 	private TileMap tileMap;
+	private Background bg;
 	
 	public Level1State(GameStateManager gsm) {
 		this.gsm = gsm;
@@ -18,6 +19,8 @@ public class Level1State extends GameState {
 		tileMap.loadMap("/Maps/level1-1.map");
 		tileMap.setPosition(0, 0);
 		
+		bg = new Background("/Backgrounds/grassbg1.gif",0.1);
+		
 	}
 	
 	
@@ -28,12 +31,16 @@ public class Level1State extends GameState {
 
 	@Override
 	public void render(Graphics2D g) {
-		//clear screen
-				g.setColor(Color.WHITE);
-				g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
-				
-				//draw tilemap
-				tileMap.draw(g);
+		/*clear screen
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);*/
+		
+		//draw bg
+		bg.draw(g);;
+		
+		//draw tilemap
+		tileMap.draw(g); 
+		
 		
 	}
 
